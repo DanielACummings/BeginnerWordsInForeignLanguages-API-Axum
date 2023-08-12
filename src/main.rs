2d514ba16283ddea8e80da218e65158076a4412f
Daniel <danielac@protonmail.com>
2023-08-12 17:01:08 -0600

    Add /language/spanish route to display Spanish words

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
-use axum::{response::IntoResponse, routing::get, Json, Router};
+use axum::{Json, response::IntoResponse, Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(display_language_options))
+        .route("/language/spanish", get(spanish));
+
+    println!("🚀 Server started successfully");
+    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
 
 async fn display_language_options() -> impl IntoResponse {
     const MESSAGE: &str =
 "Beginner words in foreign languages.
-Language options:
-    1. Spanish
-    2. German
-    3. Romanian";
+URL options:
+    /spanish
+    /german
+    /romanian";
 
     let json_response = serde_json::json!({
         "status": "success",
@@ -16,13 +29,20 @@ Language options:
     Json(json_response)
 }
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new().route("/home", get(display_language_options));
+async fn spanish() -> impl IntoResponse {
+    const MESSAGE: &str =
+"Hi/Hello: Hola
+Bye/Goodbye: Adios
+Yes: Sí
+No: No
+Please: Por favor
+Thank you: Gracias
+You're welcome: De nada";
 
-    println!("🚀 Server started successfully");
-    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let json_response = serde_json::json!({
+        "status": "success",
+        "message": MESSAGE
+    });
+
+    Json(json_response)
 }
